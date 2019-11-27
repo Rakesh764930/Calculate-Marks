a7de41d146306f8a9a7db180dac71909db196fc5
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-11-26 22:02:35 -0500

    calculated successfully

---
+++ app/src/main/java/com/example/exercise/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.exercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import java.lang.Integer;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+Button btnGoNext;
+public Integer sum=0;
+    EditText txtM1;
+    EditText txtM2;
+    EditText txtM3;
+    EditText txtM4;
+    EditText txtM5;
+    TextView result;
+TextView txtRes;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnGoNext=findViewById(R.id.btnGoNext);
+
+        txtM1=findViewById(R.id.txtM1);
+        txtM2=findViewById(R.id.txtM2);
+        txtM3=findViewById(R.id.txtM3);
+        txtM4=findViewById(R.id.txtM4);
+        txtM5=findViewById(R.id.txtM5);
+        result=findViewById(R.id.result);
+        btnGoNext.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String num1=txtM1.getText().toString();
+        final int a=Integer.parseInt(num1);
+
+        String num2=txtM2.getText().toString();
+        final int b=Integer.parseInt(num2);
+
+        String num3=txtM3.getText().toString();
+        final int c=Integer.parseInt(num3);
+
+        String num4=txtM4.getText().toString();
+        final int d=Integer.parseInt(num4);
+
+        String num5=txtM5.getText().toString();
+        final int e=Integer.parseInt(num5);
+        sum=a+b+c+d+e;
+
+
+       result.setText(String.valueOf(sum));
+        Intent mIntent=new Intent(MainActivity.this,NextActivity.class);
+        startActivity(mIntent);
+    }
+}
---
+++ app/src/main/java/com/example/exercise/Marks.java
@@ -0,0 +1,51 @@
+package com.example.exercise;
+
+public class Marks {
+    private Double m1;
+    private Double m2;
+    private Double m3;
+    private Double m4;
+    private Double m5;
+
+    public Double getM3() {
+        return m3;
+    }
+
+    public Double getM4() {
+        return m4;
+    }
+
+    public Double getM5() {
+        return m5;
+    }
+
+    public Double getM2() {
+        return m2;
+    }
+
+
+
+    public void setM1(Double m1) {
+        this.m1 = m1;
+    }
+
+    public void setM2(Double m2) {
+        this.m2 = m2;
+    }
+
+    public void setM3(Double m3) {
+        this.m3 = m3;
+    }
+
+    public void setM4(Double m4) {
+        this.m4 = m4;
+    }
+
+    public void setM5(Double m5) {
+        this.m5 = m5;
+    }
+
+    public Double getM1() {
+        return m1;
+    }
+}
